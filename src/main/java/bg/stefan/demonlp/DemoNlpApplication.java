1dfbe9f96a6a99652cbfe34b3529729fd864a073
Stefan Kolev <stefankolev@icloud.com>
2019-09-17 20:50:03 +0200

    first

---
+++ src/main/java/bg/stefan/demonlp/DemoNlpApplication.java
@@ -0,0 +1,13 @@
+package bg.stefan.demonlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNlpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/stefan/demonlp/DemoNlpApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.stefan.demonlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
