44287e009090e88516b1eb08e499d6a77a50ded5
Stefan Kolev <kolevstefan@gmail.com>
2020-03-08 00:18:01 +0100

    java11

---
+++ start.sh
@@ -0,0 +1,6 @@
+java  -Dcom.sun.management.jmxremote \
+  -Dcom.sun.management.jmxremote.port=9010 \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -jar ./target/demo-nlp-0.0.1-SNAPSHOT.jar 
